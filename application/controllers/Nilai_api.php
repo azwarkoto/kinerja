851738035e83a5ac75f41e434f3bbdfc0955f68b
the dexter <artdeffend@gmail.com>
2017-11-22 14:57:52 +0700

    REst API

--- application/controllers/Nilai_api.php
+++ application/controllers/Nilai_api.php
@@ -14,13 +14,13 @@ class Nilai_api extends REST_Controller {
 
         if ($id == '') {
             $this->db->join('penilaian', 'penilaian.kodepenilaian = nilai.kodepenilaian', 'left');
-            $this->db->join('kodeguru', 'guru.kodeguru = nilai.kodeguru', 'left');
+            $this->db->join('guru', 'guru.kodeguru = nilai.kodeguru', 'left');
             $data = $this->db->get('nilai')->result();
             // $data = array("status"=>"0");
 
         } else {
             $this->db->join('penilaian', 'penilaian.kodepenilaian = nilai.kodepenilaian', 'left');
-            $this->db->join('kodeguru', 'guru.kodeguru = nilai.kodeguru', 'left');
+            $this->db->join('guru', 'guru.kodeguru = nilai.kodeguru', 'left');
             $data = $this->db->get('nilai')->result();
             $this->db->where('kodeguru', $id);
             $data = $this->db->get('nilai')->result();
@@ -34,16 +34,12 @@ class Nilai_api extends REST_Controller {
     // insert new data to mahasiswa
     function index_post() {
         $data = array(
-            'no_customer' => $this->input->post('no_customer'),
-            'nama' => $this->input->post('nama'),
-            'alamat' => $this->input->post('alamat'),
-            'telp' => $this->input->post('telp'),
-            'date_created' => date("d-m-Y"),
-            'date_edited' => date("d-m-Y"),
-            'email' => $this->input->post('email'),
-            'password' => $this->input->post('password')
+            'kodepenilaian' => $this->input->post('kodepenilaian'),
+            'kodeguru' => $this->input->post('kodeguru'),
+            'kodeindikator' => $this->input->post('kodeindikator'),
+            'nilai' => $this->input->post('nilai')
             );
-        $insert = $this->db->insert('mahasiswa', $data);
+        $insert = $this->db->insert('nilai', $data);
         if ($insert) {
             $this->response($data, 200);
         } else {
@@ -53,39 +49,15 @@ class Nilai_api extends REST_Controller {
 
     // update data mahasiswa
     function index_put() {
-        $nim = $this->put('nim');
+        $kodepenilaian = $this->put('kodepenilaian');
         $data = array(
-                    'nim' => $this->input->post('nim'),
-        'nama' => $this->input->post('nama'),
-        'jenis_kelamin' => $this->input->post('jenis_kelamin'),
-        'agama' => $this->input->post('agama'),
-        'tempat_lahir' => $this->input->post('tempat_lahir'),
-        'tanggal_lahir' => $this->input->post('tanggal_lahir'),
-        'id_prodi' => $this->input->post('id_prodi'),
-        'alamat' => $this->input->post('alamat'),
-        'nama_ayah' => $this->input->post('nama_ayah'),
-        'nama_ibu' => $this->input->post('nama_ibu'),
-        'pekerjaan_ayah' => $this->input->post('pekerjaan_ayah'),
-        'pekerjaan_ibu' => $this->input->post('pekerjaan_ibu'),
-        'alamat_ortu' => $this->input->post('alamat_ortu'),
-        'total_penghasilan' => $this->input->post('total_penghasilan'),
-        'nohp' => $this->input->post('nohp'),
-        'tahun_masuk' => $this->input->post('tahun_masuk'),
-        'foto' => $this->input->post('foto'),
-        'password' => $this->input->post('password'),
-        'nama_sekolah' => $this->input->post('nama_sekolah'),
-        'telp_sekolah' => $this->input->post('telp_sekolah'),
-        'alamat_sekola' => $this->input->post('alamat_sekola'),
-        'jurusan_sekolah' => $this->input->post('jurusan_sekolah'),
-        'tahun_lulus' => $this->input->post('tahun_lulus'),
-        'nama_kampus' => $this->input->post('nama_kampus'),
-        'telp_kampus' => $this->input->post('telp_kampus'),
-        'alamat_kampus' => $this->input->post('alamat_kampus'),
-        'jurusan_kampus' => $this->input->post('jurusan_kampus'),
-        'tahun_lulusdiploma' => $this->input->post('tahun_lulusdiploma'),
-        'diploma' => $this->input->post('diploma'));
-        $this->db->where('nim', $nim);
-        $update = $this->db->update('mahasiswa', $data);
+            'kodepenilaian' => $this->input->post('kodepenilaian'),
+            'kodeguru' => $this->input->post('kodeguru'),
+            'kodeindikator' => $this->input->post('kodeindikator'),
+            'nilai' => $this->input->post('nilai')
+            );
+        $this->db->where('kodepenilaian', $kodepenilaian);
+        $update = $this->db->update('nilai', $data);
         if ($update) {
             $this->response($data, 200);
         } else {
@@ -95,9 +67,9 @@ class Nilai_api extends REST_Controller {
 
     // delete mahasiswa
     function index_delete() {
-        $nim = $this->delete('nim');
-        $this->db->where('nim', $nim);
-        $delete = $this->db->delete('mahasiswa');
+        $kodepenilaian = $this->delete('kodepenilaian');
+        $this->db->where('kodepenilaian', $kodepenilaian);
+        $delete = $this->db->delete('nilai');
         if ($delete) {
             $this->response(array('status' => 'success'), 201);
         } else {
